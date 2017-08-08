27db66e2c28a392e4ea6016c65973cdb3d48f9ea
Jonathan CROUZET <jonathan.crouzet.jc@gmail.com>
2017-08-08 14:28:02 +0200

    updated db manager node

--- src/db_manager_node.cpp
+++ src/db_manager_node.cpp
@@ -11,10 +11,70 @@
 
 using namespace cafer_core;
 
+class DBMNode : public DatabaseManager{
+    using DatabaseManager::DatabaseManager;
+
+public:
+
+    void init(){
+        client_connect_to_ros();
+
+        while(!add_wave("/dream_babbling/babbling")){
+            spin();
+            update();
+            sleep();
+        }
+
+        init_wave(_connected_waves.begin()->second->id);
+    }
+
+    bool init_wave(uint32_t id){
+
+        auto it = _connected_waves.find(id);
+        if(it == _connected_waves.end()) {
+            return false;
+        }
+
+        shared_ptr<DatabaseManager::_Wave> wave_3(it->second);
+
+        std::vector<std::string> string_split;
+        std::map<std::string,std::unique_ptr<ManagerQueue<DataDataset>>> dataset_managers;
+        std::map<std::string,std::unique_ptr<ManagerQueue<DataGMM>>> gmm_managers;
+        for(const auto& topic : wave_3->data_topics) {
+            boost::algorithm::split(string_split,topic.first,boost::is_any_of("_"));
+            if(string_split[0] == "dataset")
+                dataset_managers.emplace(topic.first,std::unique_ptr<ManagerQueue<DataDataset>>(
+                                             new ManagerQueue<DataDataset>));
+            else if(string_split[0] == "gmm")
+                gmm_managers.emplace(topic.first,std::unique_ptr<ManagerQueue<DataGMM>>(
+                                         new ManagerQueue<DataGMM>));
+        }
+
+        std::unique_ptr<ManagerQueue<DataController>> controller_manager{new ManagerQueue<DataController>()};
+        std::unique_ptr<ManagerQueue<DataMotionRGBD>> motion_rgbd_manager{new ManagerQueue<DataMotionRGBD>()};
+        if (wave_3 != nullptr) {
+            for(auto& manager : dataset_managers){
+                wave_3->add_manager(manager.second.release(), wave_3->data_topics[manager.first]);
+            }
+            for(auto& manager : gmm_managers){
+                wave_3->add_manager(manager.second.release(), wave_3->data_topics[manager.first]);
+            }
+
+            wave_3->add_manager(motion_rgbd_manager.release(), wave_3->data_topics["motion"]);
+            wave_3->add_manager(controller_manager.release(), wave_3->data_topics["joints_values"]);
+        }
+    }
+
+    void update(){
+        for(const auto& w : _connected_waves){
+            if(w.second->managers.empty())
+                init_wave(w.second->id);
+        }
+    }
+};
+
 int main(int argc, char** argv)
 {
-    shared_ptr<DatabaseManager::_Wave> wave_3;
-
     std::string node_name;
     XmlRpc::XmlRpcValue cafer;
 
@@ -22,47 +82,13 @@ int main(int argc, char** argv)
     cafer_core::init(argc, argv, node_name);
     cafer_core::ros_nh->getParam("cafer", cafer);
 
-    DatabaseManager db_manager(cafer["mgmt"], cafer["type"], cafer["freq"]);
+    DBMNode db_manager(cafer["mgmt"], cafer["type"], cafer["freq"]);
 
     // Retrieve WATCHDOG messages from the management topic.
     db_manager.wait_for_init();
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     db_manager.spin();
 
-    while(!db_manager.add_wave("/object_babbling/babbling")){
-        db_manager.spin();
-        db_manager.update();
-        db_manager.sleep();
-    }
-
-    db_manager.find_wave_by_name("/object_babbling/babbling", wave_3);
-
-    std::vector<std::string> string_split;
-    std::map<std::string,std::unique_ptr<ManagerQueue<DataDataset>>> dataset_managers;
-    std::map<std::string,std::unique_ptr<ManagerQueue<DataGMM>>> gmm_managers;
-    for(const auto& topic : wave_3->data_topics) {
-        boost::algorithm::split(string_split,topic.first,boost::is_any_of("_"));
-        if(string_split[0] == "dataset")
-            dataset_managers.emplace(topic.first,std::unique_ptr<ManagerQueue<DataDataset>>(
-                                         new ManagerQueue<DataDataset>));
-        else if(string_split[0] == "gmm")
-            gmm_managers.emplace(topic.first,std::unique_ptr<ManagerQueue<DataGMM>>(
-                                     new ManagerQueue<DataGMM>));
-    }
-
-    std::unique_ptr<ManagerQueue<DataController>> controller_manager{new ManagerQueue<DataController>()};
-    std::unique_ptr<ManagerQueue<DataMotionRGBD>> motion_rgbd_manager{new ManagerQueue<DataMotionRGBD>()};
-    if (wave_3 != nullptr) {
-        for(auto& manager : dataset_managers){
-            wave_3->add_manager(manager.second.release(), wave_3->data_topics[manager.first]);
-        }
-        for(auto& manager : gmm_managers){
-            wave_3->add_manager(manager.second.release(), wave_3->data_topics[manager.first]);
-        }
-
-        wave_3->add_manager(motion_rgbd_manager.release(), wave_3->data_topics["motion"]);
-        wave_3->add_manager(controller_manager.release(), wave_3->data_topics["joints_values"]);
-    }
 
     while (ros::ok() && (!db_manager.get_terminate())) {
         db_manager.spin();
